4af055f24a7ca0152f385ee6f931a7eb496021e0
Sathishkumar <satheezz.p@gmail.com>
2025-12-25 21:30:18 +0530

    fix: Correct comment syntax for Google AdSense integration in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
           `
         }} />
         
-        /* Google AdSense */
+        {/* Google AdSense */}
         {process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID && (
           <>
             <script
